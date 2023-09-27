949af22aa373bb0f91b6c9c7498188030a5539e1
Rob Gering <rob.gering@snu.edu>
2023-09-27 13:12:33 -0500

    Clean up RegExp examples

--- syntax/14 regexes.js
+++ syntax/14 regexes.js
@@ -9,17 +9,20 @@
  *
  **/
 
-// extract email address
-const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+// Regexp to extract an email address
+const emailRegex = /[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/g;
 
+// testing a RegExp
 console.log(emailRegex.test("test@example.com")); // should return true
 console.log(emailRegex.test("invalid_email@com")); // should return false
 
-// extract phone number
-const simplePhoneRegex = /^(\d{10}|\d{3}[-.]\d{3}[-.]\d{4})$/;
+// using a RegExp in a String method
+const maybeEmails = "My emails are foo@example.com and bar@example.com";
+email_addresses = maybeEmails.match(emailRegex);
+console.log(email_addresses);
 
-console.log(simplePhoneRegex.test("405-555-5555")); // should return true
-console.log(simplePhoneRegex.test("400332aaaaaa")); // should return false
+// validate a phone number
+const simplePhoneRegex = /^(\d{10}|\d{3}[-.]\d{3}[-.]\d{4})$/;
 
 // extract every instance of the names Carol, Carlos, and Carl
 const namesRegex = /\b(?:Carol|Carlos|Carl)\b/gi;
@@ -33,17 +36,17 @@ const dateRegex = /\b\d{2}[-/]\d{2}[-/]\d{4}\b/g;
 // passwords that are 8 characters long, one lowercase and uppercase letter
 const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
 
-// extracting hashtags from a tweet
+// extract hashtags from a tweet
 const hashtagRegex = /#\w+/g;
 
-// extracting URLs from text
+// extract URLs from text
 const urlRegex = /https?:\/\/[^\s]+/g;
 
-// validating a single postal code
+// validate a single postal code
 const zipCodeRegex = /^\d{5}(-\d{4})?$/;
 
-// extracting image file names
+// extract image file names
 const imageFileRegex = /\b[\w/-]+\.(jpg|jpeg|png|gif)\b/g;
 
-// extracting decimal numbers from text
+// extract decimal numbers from text
 const decimalRegex = /\b\d+\.\d+\b/g;
