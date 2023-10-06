ef08068783f3b17e4fbfc2813db316de1883aa19
Rob Gering <rob.gering@snu.edu>
2023-10-06 09:48:00 -0500

    Add examples of callbacks and Promises

--- syntax/16 asynchrony.js
+++ syntax/16 asynchrony.js
@@ -22,7 +22,7 @@ function readFiles() {
 
 readFiles();
 
-// Asynchronous example
+// Asynchronous example (ES2017+)
 async function readFilesAsync() {
     try {
         // We can only use `await` in functions marked `async`
@@ -34,3 +34,74 @@ async function readFilesAsync() {
 }
 
 readFilesAsync();
+
+/**
+ * In earlier versions of JavaScript, it was common to nest "callback" functions
+ * to run when a condition was met or when an exception was raised. This would
+ * quickly devolve into an anti-pattern called "The Pyramid of Doom".
+ *
+ * Here is a trivial example that attempts to read a file, resorting to backup
+ * files if the first file is not present. These callback chains are hard to
+ * read, as code will execute out of order depending on the errors raised.
+ */
+
+fs.readFile("file1.txt", "utf8", function (err, data1) {
+    if (err) {
+        console.error("Error reading file1:", err);
+        fs.readFile("file2.txt", "utf8", function (err, data2) {
+            if (err) {
+                console.error("Error reading file2:", err);
+                fs.readFile("file3.txt", "utf8", function (err, data3) {
+                    if (err) {
+                        console.error("Error reading file3:", err);
+                        // And so on...
+                    } else {
+                        console.log("File3 contents:", data3);
+                    }
+                });
+            } else {
+                console.log("File2 contents:", data2);
+            }
+        });
+    } else {
+        console.log("File1 contents:", data1);
+    }
+});
+
+/**
+ * The first solution to the callback solution was the use of Promises. Promises
+ * are like prototypes in that they are still used behind-the-scenes, even
+ * though we have better syntax today. Promises are worth understanding.
+ */
+
+// This function returns a Promise
+function readFilePromise(filename) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filename, "utf8", (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    });
+}
+
+// Using a Promise
+readFilePromise("LICENSE")
+    // We will still encounter some nesting if we attempt to read multiple files
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((err) => {
+        console.error("Error reading file:", err);
+    });
+
+/**
+ * Now go back to the top of the file and look at the async/await example.
+ *
+ * This syntax was introduced to make it easier to think about asynchronous
+ * code.  Although it is asynchronous, the use of the `await` keyword reads very
+ * similarly to synchronous code. As long as we are aware that things may happen
+ * out of order, this code is easier to read.
+ */
